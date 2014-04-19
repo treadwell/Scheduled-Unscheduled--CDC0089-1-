9899059b5bbbaffd49fe67582f9f61b237e75717
treadwell <brooks.ken@gmail.com>
2014-04-19 08:42:20 -0400

    Updates data

--- run_reports.py
+++ run_reports.py
@@ -42,3 +42,25 @@ Ashland.summary()
 Groveport.summary()
 Ryerson.summary()
 TotalDomestic.summary()
+
+# -------------- Plot YTD comparisons ---------------
+
+Gahanna.plot_ytd_comparison('new_dollars')
+Gahanna.plot_ytd_comparison('new_orders')
+Gahanna.plot_ytd_comparison('new_lines')
+Gahanna.plot_ytd_comparison('new_units')
+print "-----------------"
+Ashland.plot_ytd_comparison('new_dollars')
+Ashland.plot_ytd_comparison('new_orders')
+Ashland.plot_ytd_comparison('new_lines')
+Ashland.plot_ytd_comparison('new_units')
+print "-----------------"
+Groveport.plot_ytd_comparison('new_dollars')
+Groveport.plot_ytd_comparison('new_orders')
+Groveport.plot_ytd_comparison('new_lines')
+Groveport.plot_ytd_comparison('new_units')
+print "-----------------"
+Ryerson.plot_ytd_comparison('new_dollars')
+Ryerson.plot_ytd_comparison('new_orders')
+Ryerson.plot_ytd_comparison('new_lines')
+Ryerson.plot_ytd_comparison('new_units')
